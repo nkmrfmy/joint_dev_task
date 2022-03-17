55d51cc9f7ab2abe7e3b0ae133b67a622fc6a8c6
FMY <n.fmy.0526@icloud.com>
2022-03-17 22:10:57 +0900

    Q7~Q9を解答

--- task.rb
+++ task.rb
@@ -56,7 +56,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-
+  array.map!(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
@@ -65,7 +65,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -75,7 +76,9 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names.each.with_index(1) do|name,i|
+    puts "会員No.#{i} #{name}さん"
+  end
 end
 
 def q10
