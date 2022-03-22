7c01f3b34fc571cffb8eded438d1ebadefd763ce
FMY <n.fmy.0526@icloud.com>
2022-03-22 21:07:27 +0900

    Q10 修正

--- task.rb
+++ task.rb
@@ -85,12 +85,13 @@ def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-  if foods.include?("うに")
-    puts "好物です"
-  else
-    puts "まぁまぁ好きです"
+  foods.each do |food|
+    if food.include?("うに")
+      puts "#{food}：好物です"
+    else
+      puts "#{food}：まぁまぁ好きです"
+    end
   end
-  
 end
 
 def q11
