0ebb5bba259c136fef7e36a1356b7f4c7a31867f
FMY <n.fmy.0526@icloud.com>
2022-03-23 21:06:28 +0900

    Q14~Q16を解答

--- task.rb
+++ task.rb
@@ -125,7 +125,8 @@ def q14
   data = { name: "satou", age: 33, address: "saitama", hobby: "soccer", email: "hoge@fuga.com" }
 
   # 以下に回答を記載
-
+  key = data.keys
+  p key
 end
 
 def q15
@@ -133,7 +134,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-
+puts data1[:age]? "OK" : "NG"
+puts data2[:age]? "OK" : "NG"
 end
 
 def q16
@@ -145,7 +147,10 @@ def q16
   ]
 
   # 以下に回答を記載
+  users.each do |user|
+    puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です。"
 
+  end
 end
 
 class UserQ17
